15aa7aac4564acb4a2560682fbac14b6784d5b2c
dulbadrakh <jumpdol@gmail.com>
2021-02-18 20:50:18 +0800

    first commit

---
+++ archnodejs/app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Post = require("./models/Post");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+var cors = require("cors");
+const checkInternetConnected = require("check-internet-connected");
+
+require("dotenv").config({ path: __dirname + "/.env" });
+
+const config = {
+  timeout: 5000, //timeout connecting to each server, each try
+  retries: 5, //number of retries to do before failing
+  domain: "https://www.google.com", //the domain to check DNS record of
+};
+
+setInterval(() => {
+  checkInternetConnected(config)
+    .then(() => {
+      fs.readFile("myjson.json", "utf8", (err, data) => {
+        let datas = `{
+          "requests":[
+              
+          ]
+      }`;
+        jsons = JSON.parse(data);
+        if (err) throw err;
+        if (jsons.requests.length > 0) {
+          fs.writeFile("myjson.json", datas, "utf-8", (err) => {
+            console.log(err);
+            if (err) throw err;
+          });
+          for (let i = 0; i < jsons.requests.length; i++) {
+            const post = new Post({
+              title: jsons.requests[i].title,
+              description: jsons.requests[i].description,
+            });
+            post.save();
+          }
+        }
+      }); //successfully connected to a server
+    })
+    .catch((ex) => {
+      console.log("Offline"); // cannot connect to a server or error occurred.
+    });
+}, 5000);
+
+app.use(bodyParser.json());
+app.use(cors());
+
+//Import routes
+const postsRoute = require("./routes/posts");
+
+app.use("/posts", postsRoute);
+
+// ROUTES
+// app.use("/posts", () => {
+//   console.log("This is a middleware running");
+// });
+
+app.get("/", (req, res) => {
+  res.send("We are on home");
+});
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to DB");
+  }
+);
+
+app.listen(3000);
---
+++ archnodejs/routes/posts.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post");
+const fs = require("fs");
+const checkInternetConnected = require("check-internet-connected");
+
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+router.get("/specific", (req, res) => {
+  res.send("Specific post");
+});
+
+router.post("/", async (req, res) => {
+  checkInternetConnected()
+    .then((result) => {
+      const post = new Post({
+        title: req.body.title,
+        description: req.body.description,
+      });
+      try {
+        const savedPost = post.save();
+        res.json(savedPost);
+      } catch (err) {
+        res.json({ message: err });
+      }
+    })
+    .catch((ex) => {
+      fs.readFile("myjson.json", "utf8", (err, data) => {
+        if (err) throw err;
+        var data = JSON.parse(data);
+        data.requests.push(req.body);
+        fs.writeFile("myjson.json", JSON.stringify(data), "utf-8", (err) => {
+          if (err) throw err;
+        });
+      });
+      res.json({ message: "err" });
+    });
+});
+
+router.post("/delete", (req, res) => {
+  Post.find({ title: req.body.id.replace(/\-/g, " ") }).deleteMany(
+    (err, data) => {
+      if (err) throw err;
+      res.json(data);
+    }
+  );
+  res.json(req.body);
+});
+
+module.exports = router;
